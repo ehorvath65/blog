6f5eee8229bfa276ab59a703787f2097199c4174
lbarjak <barjak.laszlo@gmail.com>
2020-06-04 17:39:13 +0200

    Thymeleaf variables to css style with javascript

--- src/main/java/blog/service/CatService.java
+++ src/main/java/blog/service/CatService.java
@@ -13,7 +13,6 @@ import blog.repo.CatRepository;
 public class CatService {
 
 	private CatRepository catRepo;
-	LinkedHashMap<String, Cat> completeCats = new LinkedHashMap<>();
 
 	@Autowired
 	public void setCatRepo(CatRepository catRepo) {
@@ -21,6 +20,7 @@ public class CatService {
 	}
 
 	public LinkedHashMap<String, Cat> getCompleteCats() {
+		LinkedHashMap<String, Cat> completeCats = new LinkedHashMap<>();
 		List<Cat> items = catRepo.findAll();
 		for (Cat item : items) {
 			completeCats.put(item.getCat(), item);
